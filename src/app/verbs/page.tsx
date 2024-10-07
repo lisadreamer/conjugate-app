369e87e8073f1dc246aa417e56a0ca3f6aacd615
lisadreamer <lisadreamer7@gmail.com>
2024-10-07 20:21:11 +0300

    show verbs list

--- src/app/verbs/page.tsx
+++ src/app/verbs/page.tsx
@@ -6,6 +6,12 @@ import verbImg from "public/images/performance.jpg";
 export default async function VerbsPage() {
   const verbs = await db.verb.findMany();
 
+  const verbsList = verbs.map((verb) => (
+    <div key={verb.id} className="flex p-4 bg-blue-100 rounded shadow-lg">
+      <h3 className="text-xl text-gray-700">{verb.title}</h3>
+    </div>
+  ));
+
   return (
     <div>
       <Hero
@@ -13,7 +19,11 @@ export default async function VerbsPage() {
         imgAlt="welding"
         imgData={verbImg}
       />
-      <Link href="/verbs/new">Create</Link>
+      <div className="flex flex-col gap-4 justify-center items-center p-8">
+        <h2 className="text-2xl font-bold">Verbs:</h2>
+        <div className="flex flex-wrap gap-4 pb-2">{verbsList}</div>
+        <Link href="/verbs/new" className="bg-green-500 rounded px-4 py-2 text-white">Create</Link>
+      </div>
     </div>
   );
 }
