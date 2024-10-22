6f542098a67a94b9bb06e1254b9e7b29c722522a
lisadreamer <lisadreamer7@gmail.com>
2024-10-22 13:18:51 +0300

    Add GitHub OAuth with NextAuth and Prisma integration
    Implemented authentication with GitHub using NextAuth in `src/auth.ts`. Added Prisma models for managing users, accounts, and sessions. Updated `layout.tsx` to wrap the application in a `Providers` component for session management and included necessary dependencies in `package.json`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header";
+import Providers from "@/app/providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,8 +32,10 @@ export default function RootLayout(
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
