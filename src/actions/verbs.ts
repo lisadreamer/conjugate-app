d43e8486c95fe09bedc8944e55a3df198776dfa9
lisadreamer <lisadreamer7@gmail.com>
2024-10-23 16:49:46 +0300

    Implement form validation and error handling for creating verbs
    Added Zod for schema validation in the `createVerb` function to ensure form inputs meet requirements. Updated `VerbCreatePage` to display error messages using NextUI components.

--- src/actions/verbs.ts
+++ src/actions/verbs.ts
@@ -1,55 +1,71 @@
-"use server";
+'use server'
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import { db } from "@/db";
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+import { db } from '@/db'
+import { Verb } from '@prisma/client'
+
+interface CreateVerbFormState {
+  errors: {
+    title?: string[]
+    description?: string[]
+    _form?: string[]
+  }
+}
+
+const createVerbSchema = z.object({
+  title: z.string().min(3),
+  description: z.string().min(5),
+})
 
 export async function createVerb(
-  formState: { message: string },
+  formState: CreateVerbFormState,
   formData: FormData
-) {
+): Promise<CreateVerbFormState> {
+  const result = createVerbSchema.safeParse({
+    title: formData.get('title'),
+    description: formData.get('description'),
+  })
+
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors }
+  }
+
+  let verb: Verb
   try {
-    const title = formData.get("title");
-    const description = formData.get("description");
-
-    if (typeof title !== "string" || title.length < 3) {
-      return { message: "Title must be longer" };
-    } else if (typeof description !== "string" || description.length < 3) {
-      return { message: "Description must be longer" };
-    }
-
-    await db.verb.create({
+    verb = await db.verb.create({
       data: {
-        title,
-        description,
+        title: result.data.title,
+        description: result.data.description,
       },
-    });
+    })
     /*throw new Error("Not implemented"); // to test error handling manually */
   } catch (error: unknown) {
     if (error instanceof Error) {
-      return { message: error.message };
+      return { errors: { _form: [error.message] } }
     } else {
-      return { message: "An unknown error occurred" };
+      return { errors: { _form: ['An unknown error occurred'] } }
     }
   }
 
-  revalidatePath("/verbs");
-  redirect("/verbs");
+  revalidatePath('/verbs')
+  redirect(`/verbs/${verb.id}`)
 }
 
 export async function updateVerb(id: number, description: string) {
   await db.verb.update({
     where: { id },
     data: { description },
-  });
-  revalidatePath(`/verbs/${id}`);
-  redirect(`/verbs/${id}`);
+  })
+  revalidatePath(`/verbs/${id}`)
+  redirect(`/verbs/${id}`)
 }
 
 export async function deleteVerb(id: number) {
   await db.verb.delete({
     where: { id },
-  });
-  revalidatePath("/verbs");
-  redirect("/verbs");
+  })
+  revalidatePath('/verbs')
+  redirect('/verbs')
 }
--- src/app/verbs/new/page.tsx
+++ src/app/verbs/new/page.tsx
@@ -1,54 +1,48 @@
-"use client";
+'use client'
 
-import { useFormState } from "react-dom";
-import { createVerb } from "@/actions";
+import { useFormState } from 'react-dom'
+import { Input, Textarea, Button } from '@nextui-org/react'
+import { createVerb } from '@/actions'
 
 export default function VerbCreatePage() {
-  const [formState, action] = useFormState(createVerb, { message: "" });
+  const [formState, action] = useFormState(createVerb, { errors: {} })
 
   return (
-    <div className="px-40 py-40 flex flex-col gap-8">
+    <div className="mx-80 py-40 flex flex-col gap-8">
       <form action={action}>
         <h1 className="text-2xl my-5">Create a verb</h1>
         <div className="flex flex-col gap-4">
-          <div className="flex gap-4">
-            <label className="w-28" htmlFor="title">
-              Title
-            </label>
-            <input
-              id="title"
-              name="title"
-              className="border rounded p-2 w-full"
-            />
-          </div>
+          <Input
+            name="title"
+            label="Title"
+            labelPlacement="outside"
+            placeholder="Title"
+            isInvalid={!!formState.errors.title}
+            errorMessage={formState.errors.title?.join(', ')}
+          />
 
-          <div className="flex gap-4">
-            <label className="w-28" htmlFor="description">
-              Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              className="border rounded p-2 w-full"
-            />
-          </div>
+          <Textarea
+            name="description"
+            label="Description"
+            labelPlacement="outside"
+            placeholder="Describe verb..."
+            isInvalid={!!formState.errors.description}
+            errorMessage={formState.errors.description?.join(', ')}
+          />
 
-          {formState.message ? (
+          {formState.errors._form ? (
             <div className="bg-red-200 border rounded border-red-400 p-2 ">
-              {formState.message}
+              {formState.errors._form?.join(', ')}
             </div>
           ) : null}
 
           <div className="flex justify-end">
-            <button
-              type="submit"
-              className="rounded bg-green-500 py-2 px-4 text-white"
-            >
+            <Button type="submit" color="success" className="text-white">
               Create
-            </button>
+            </Button>
           </div>
         </div>
       </form>
     </div>
-  );
+  )
 }
